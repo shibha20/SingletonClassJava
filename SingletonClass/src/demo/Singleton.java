7a9c2cd1e8aa2610cc26db032bd52ecfd93760e6
Shilpa <parasianshilpa@gmail.com>
2022-05-10 11:51:31 +0545

    fixing spaching

--- SingletonClass/src/demo/Singleton.java
+++ SingletonClass/src/demo/Singleton.java
@@ -1,26 +1,20 @@
 package demo;
 
 public class Singleton {
-
 	public static void main(String[] args) {
 		Abc obj1 = Abc.getInstance();
 	}
-
 }
-
-
 
 
 class Abc
 {
-	
 	static Abc obj = new Abc();
 	
 	//private constructor so that a new instance of a class can not be created
 	private Abc() {
 		
 	}
-	
 	
 	//method to return only one instance of the class 
 	public static Abc getInstance() {
